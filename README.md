02e967cd5f55ca89dc24c65691389bec67db016f
markmarijnissen <1287693+markmarijnissen@users.noreply.github.com>
2022-12-12 19:05:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# invert-camera
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-wjmpmh)
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <canvas id="canvas"></canvas>
+</template>
+<style scoped>
+canvas {
+  width: 98vw;
+  height: 98vh;
+  object-fit: contain;
+}
+</style>
+<script setup>
+import Instacam from 'instacam';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  let camera = new Instacam(document.querySelector('#canvas'));
+  camera.invert = 1;
+});
+</script>
